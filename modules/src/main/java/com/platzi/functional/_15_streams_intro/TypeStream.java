e6bb042bdebacd8efae8414891db14ee8b9f1df2
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-30 14:39:46 -0600

    Streams Update

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,6 +1,10 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TypeStream {
     public static void main(String[] args) {
@@ -9,5 +13,18 @@ public class TypeStream {
                 .parallel()
                 .filter(x-> x % 2 == 0)
                 .forEach(System.out::println);
+
+
+        IntStream infiniteStream2 = IntStream.iterate(0, x-> x+1);
+        List<Integer> numberList = infiniteStream2.limit(1000)
+                .filter(x-> x % 2 == 0)
+                .boxed()
+                .collect(Collectors.toList());
+
+
+
     }
-}
+    }
+
+
+
